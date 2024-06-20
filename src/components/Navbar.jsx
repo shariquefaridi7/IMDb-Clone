88f77ae541604631bfacc2dfe047bf7ce485ce5a
Mohd Sharique <76559067+shariquefaridi7@users.noreply.github.com>
2024-06-21 03:05:25 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 
 import NavbarItem from './NavbarItem'
+import {Suspense} from "react";
 
 const Navbar = () => {
     return (
         <div className='flex gap-6 text-xl p-4 justify-center mb-4 bg-amber-100 dark:bg-gray-600'>
-
+<Suspense>
             <NavbarItem name='Trending' param='fetchTrending' />
             <NavbarItem name='Top Rated' param='fetchTopRated' />
+    </Suspense>
         </div>
     )
 }
