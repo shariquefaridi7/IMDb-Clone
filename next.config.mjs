e3843e9043f6749bde7692f97d512c35512c2174
Mohd Sharique <76559067+shariquefaridi7@users.noreply.github.com>
2024-06-21 18:34:17 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'image.tmdb.org',
-                pathname: '**',
+                hostname: '**',
+                
             }
         ]
     }
